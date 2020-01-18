ac63dc5233a2b6690194d82359a524e3e4f7826c
Koustav Banerjee <52373756+Kens3i@users.noreply.github.com>
2020-01-18 13:24:31 +0530

    Slide project and application
    A basic game i made, a level based obstacle runner

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : UnityEngine.MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+
+  public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("restart", restartDelay);
+        }
+    }
+
+    void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float fwd;
+    public float side;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(fwd,0,0 );
+
+        if (Input.GetKey("a")) 
+        {
+            rb.AddForce(0,0,side,ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, -side, ForceMode.VelocityChange);
+        }
+        if(rb.position.y<-2f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
